41cbbb660f73c2d10468858e7d59a8f660db3825
Nguyen Truong Bach <bachnguyentruong2003@gmail.com>
2024-04-03 10:53:17 +0700

    [add] lab-01 ex01 ex02

---
+++ lab_01/ex02_05.py
@@ -0,0 +1,17 @@
+# Nhập số giờ làm mỗi tuần
+so_gio_lam = float(input("Nhập số giờ làm mỗi tuần: "))
+
+# Nhập thù lao trên mỗi giờ làm tiêu chuẩn
+luong_gio = float(input("Nhập thù lao trên mỗi giờ làm tiêu chuẩn: "))
+
+# Số giờ làm chuẩn mỗi tuần
+gio_tieu_chuan = 44
+
+# Số giờ làm vượt chuẩn mỗi tuần
+gio_vuot_chuan = max(0, so_gio_lam - gio_tieu_chuan)
+
+# Tính tổng thu nhập
+thuc_linh = gio_tieu_chuan * luong_gio + gio_vuot_chuan * luong_gio * 1.5
+
+# In ra màn hình số tiền thực lĩnh của nhân viên
+print(f"Số tiền thực lĩnh của nhân viên: {thuc_linh}")

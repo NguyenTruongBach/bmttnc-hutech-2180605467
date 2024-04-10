9644569ee43a435ffcc8c122394f96a8ade129af
Nguyen Truong Bach <bachnguyentruong2003@gmail.com>
2024-04-10 23:40:58 +0700

    [add] lab_02 playfair

--- lab_02/cipher/railfence/railfence_cipher.py
+++ lab_02/cipher/railfence/railfence_cipher.py
@@ -1,14 +1,14 @@
 class RailFenceCipher:
     
-    def __init__(self):
+    def init (self):
         pass
-
-    def rail_fence_encrypt(self, plain_text, num_rails):
-        rails = [[] for _ in range(num_rails)]
+    
+    def rail_fence_encrypt (self, plain_text, num_rails):
+        rails = [[] for _ in range (num_rails)]
         rail_index = 0
-        direction = 1  # 1: down, -1: up
+        direction = 1 #1: down, -1: up
         for char in plain_text:
-            rails[rail_index].append(char)
+            rails[rail_index].append (char)
             if rail_index == 0:
                 direction = 1
             elif rail_index == num_rails - 1:
@@ -16,36 +16,38 @@ class RailFenceCipher:
             rail_index += direction
         cipher_text = ''.join(''.join(rail) for rail in rails)
         return cipher_text
-
+    
     def rail_fence_decrypt(self, cipher_text, num_rails):
-        rail_lengths = [0] *num_rails
+        rail_lengths = [0] * num_rails
         rail_index = 0
         direction = 1
-        
+
         for _ in range(len(cipher_text)):
-            rail_lengths[rail_index] += 1
+            rail_lengths[rail_index] += 1  # Fix: Increment rail_lengths[rail_index]
             if rail_index == 0:
                 direction = 1
             elif rail_index == num_rails - 1:
                 direction = -1
             rail_index += direction
-            
+
         rails = []
         start = 0
         for length in rail_lengths:
             rails.append(cipher_text[start:start + length])
             start += length
-            plain_text = ""
-            rail_index = 0
-            direction = -1
 
-            for _ in range(len(rails[0])):
+        plain_text = ""
+        rail_index = 0
+        direction = 1
+
+        for _ in range(len(cipher_text)):
+            if len(rails[rail_index]) > 0:  # Ensure there are characters left in the rail
                 plain_text += rails[rail_index][0]
                 rails[rail_index] = rails[rail_index][1:]
-                if rail_index == 0:
-                    direction = 1
-                elif rail_index == num_rails - 1:
-                    direction = -1
-                rail_index += direction
+            if rail_index == 0:
+                direction = 1
+            elif rail_index == num_rails - 1:
+                direction = -1
+            rail_index += direction
 
-            return plain_text
+        return plain_text
